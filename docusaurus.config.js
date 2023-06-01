ef2f7358beafd7713e6f0a6aa8b0d3eb2c34935d
imagine10255 <imagine10255@gmail.com>
2023-06-01 21:48:19 +0800

    modify some text

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Bear React Carousel',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -64,9 +64,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Bear React Carousel',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Bear React Carousel Logo',
           src: 'img/logo.svg',
         },
         items: [
@@ -74,7 +74,13 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Guide',
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Config',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
