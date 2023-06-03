a319fb69a3fd862ae2a4fdff62084c4016edfda9
imagine10255 <imagine10255@gmail.com>
2023-06-03 18:19:27 +0800

    format doc title

--- src/components/Carousel/data.tsx
+++ src/components/Carousel/data.tsx
@@ -21,3 +21,23 @@ export const imageData = images.map(row => {
         children: <BearSlideItem as="image" imageUrl={row.image}/>
     };
 });
+
+
+const numberColors = [
+    {id: 1, color: 'green'},
+    {id: 2, color: 'blue'},
+    {id: 3, color: 'gray'},
+    {id: 4, color: 'white'},
+    {id: 5, color: 'black'},
+    {id: 6, color: 'yellow'},
+    {id: 7, color: 'purple'},
+    {id: 8, color: 'red'},
+];
+export const colorData = numberColors.map(row => {
+    return {
+        key: row.id,
+        children: <BearSlideItem>
+            <div style={{height: '100%', backgroundColor: row.color}}/>
+        </BearSlideItem>
+    };
+});
