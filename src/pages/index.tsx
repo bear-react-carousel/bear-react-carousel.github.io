7379dea19dff4ef19f73e7f954caee70ce2988fa
imagine10255 <imagine10255@gmail.com>
2023-06-04 10:58:44 +0800

    modify link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/why">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
