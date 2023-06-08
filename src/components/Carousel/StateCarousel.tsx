4c6627462e243f6e93a770a9c22c17db147c2c3a
imagine10255 <imagine10255@gmail.com>
2023-06-08 10:01:08 +0800

    modify update-slide-content

--- src/components/Carousel/StateCarousel.tsx
+++ src/components/Carousel/StateCarousel.tsx
@@ -5,14 +5,14 @@ import BrowserOnly from '@docusaurus/BrowserOnly';
 
 
 const StateCarousel = () => {
-    const [activeId, setActiveId] = React.useState(0);
+    const [activeId, setActiveId] = React.useState(1);
     const myData = React.useMemo(() => images.map(row => {
         const isActive = row.id === activeId;
         return {
             key: row.id,
             children: <BearSlideImage
                      imageUrl={row.image} onClick={() => setActiveId(row.id)}
-                     style={isActive ? {transition: 'filter .4s', filter: 'blur(4px)'}: undefined}
+                     style={!isActive ? {transition: 'filter .4s', filter: 'blur(9px)'}: undefined}
             >
             </BearSlideImage>
         };
@@ -24,7 +24,7 @@ const StateCarousel = () => {
                 data={myData}
                 slidesPerView={3}
                 isEnableNavButton
-                height={{ widthRatio: 16, heightRatio: 9 }}
+                height={{ widthRatio: 32, heightRatio: 9 }}
                 isSlideItemMemo
             />
         }}
