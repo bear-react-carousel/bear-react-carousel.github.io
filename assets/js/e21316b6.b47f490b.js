3c8fa7f3efbe0856a5a358f43a6f2073df9f25a1
imagine10255 <imagine10255@gmail.com>
2023-06-14 21:34:52 +0800

    Deploy website - based on 5cb09cd1f1f7a3068ba45392f188f156fc3503bf

---
+++ assets/js/e21316b6.b47f490b.js
@@ -0,0 +1,75 @@
+"use strict";(self.webpackChunkbear_react_carousel_doc=self.webpackChunkbear_react_carousel_doc||[]).push([[1552],{531:(e,t,a)=>{a.d(t,{Z:()=>o});const o={gridGutterWidth:10,gridColumns:24,gridBreakpoints:{xs:0,sm:576,md:768,lg:992,xl:1200,xxl:1540},containerMaxWidths:{sm:540,md:720,lg:960,xl:1140,xxl:1540},gridGutterWidthMedia:{sm:10,md:10,lg:10,xl:10,xxl:10}}},6659:(e,t,a)=>{a.d(t,{Q:()=>o});const o=[{id:1,subTitle:"MEAT & SEAFOOD",title:"FROM NORWAY",desc:"We offer huge variety of execeptionally fresh meat farmed in our personal farm",imageUrl:a(9578).Z,position:"right"},{id:2,subTitle:"WILD SALMON",title:"CUTTING & DELIVERY",desc:"We offer huge variety of execeptionally fresh meat farmed in our personal farm",imageUrl:a(8878).Z,position:"right"},{id:3,subTitle:"CUTTING & DELIVERY",title:"QUALITY MONITORING",desc:"We offer huge variety of execeptionally fresh meat farmed in our personal farm",imageUrl:a(3882).Z,position:"left"}]},4650:(e,t,a)=>{a.r(t),a.d(t,{assets:()=>P,contentTitle:()=>x,default:()=>w,frontMatter:()=>f,metadata:()=>y,toc:()=>k});var o=a(7462),r=a(7294),i=a(3905),s=a(9521),l=a(5962),n=a(531),d=a(4040),p=a(1262),c=a(6659);const m=c.Q.map((e=>({key:e.id,children:r.createElement(l.B2,{bgUrl:e.imageUrl})}))),u=e=>{let{isLoadData:t=!0}=e;return r.createElement(d.tC,{gridTheme:n.Z},r.createElement(h,{className:"mb-4 mb-lg-5"},r.createElement(p.Z,null,(()=>r.createElement(l.ZP,{data:t?m:void 0,renderPagination:e=>c.Q.map((e=>r.createElement(b,{key:e.id},e.title))),slidesPerView:1,slidesPerGroup:1,isEnablePagination:!0,isEnableNavButton:!0,isEnableLoop:!0,autoPlayTime:5e3,isEnableAutoPlay:!0,height:{widthRatio:16,heightRatio:9},breakpoints:{576:{isEnablePageContent:!0,height:{widthRatio:32,heightRatio:9}}}})))))},g=s.F4`
+    from {
+        width: 0;
+    }
+
+    to {
+        width: 100%;
+    }
+`,b=s.ZP.div`
+    padding: 10px 20px;
+`,h=s.ZP.div`
+    .${l.eb.paginationContent}{
+        background-color: rgba(255, 255, 255, .8);
+    }
+
+    ${e=>s.iv`
+      
+       ${d.BC.sm`
+           .${l.eb.paginationGroup}{
+                bottom: 0;
+                width: 100%;
+                color: #000;
+                overflow: hidden;
+            }
+            .${l.eb.paginationButton}{
+                &:after {
+                    content: '';
+                    width: 0;
+                    height: 4px;
+                    background-color: greenyellow;
+                    position: absolute;
+                    bottom: 0;
+                    left: 0;
+                }
+        
+                &[data-active=true]:after {
+                    animation: ${g} ${5e3}ms linear infinite;
+                    animation-iteration-count: 1;
+                }
+            }
+       `}
+       ${d.BC.xl`
+            .${l.eb.paginationGroup}{
+                bottom: -35px;
+                background-color: #fff;
+                height: 70px;
+                width: 70%;
+                margin: auto;
+                display: flex;
+                box-shadow: none;
+            }
+    
+             .${l.eb.paginationButton}{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100%;
+                font-size: 12px;
+                font-weight: bold;
+                color: #000;
+                position: relative;
+                flex: 1;
+                background: transparent;
+                border: solid 1px #ededed;
+                border-radius: 0;
+                margin: 0;
+                padding: 0;
+                box-shadow: none;
+    
+            }
+    
+        `}
+    `}   
+    
+`,f={},x="Auto Play Progress",y={unversionedId:"examples/auto-play-progress",id:"examples/auto-play-progress",title:"Auto Play Progress",description:"The implementation code is here",source:"@site/docs/examples/auto-play-progress.mdx",sourceDirName:"examples",slug:"/examples/auto-play-progress",permalink:"/docs/examples/auto-play-progress",draft:!1,editUrl:"https://github.com/bear-react-carousel/bear-react-carousel.github.io/blob/main/docs/examples/auto-play-progress.mdx",tags:[],version:"current",frontMatter:{},sidebar:"tutorialSidebar",previous:{title:"Examples",permalink:"/docs/category/examples"},next:{title:"Deposit Tab",permalink:"/docs/examples/deposit-tab"}},P={},k=[],E={toc:k},T="wrapper";function w(e){let{components:t,...a}=e;return(0,i.kt)(T,(0,o.Z)({},E,a,{components:t,mdxType:"MDXLayout"}),(0,i.kt)("h1",{id:"auto-play-progress"},"Auto Play Progress"),(0,i.kt)(u,{mdxType:"AutoPlayProgressCarousel"}),(0,i.kt)("p",null,"The implementation code is ",(0,i.kt)("a",{parentName:"p",href:"https://github.com/bear-react-carousel/bear-react-carousel.github.io/blob/main/src/examples/AutoPlayProgressCarousel/AutoPlayProgressCarousel.tsx"},"here")))}w.isMDXComponent=!0},9578:(e,t,a)=>{a.d(t,{Z:()=>o});const o=a.p+"assets/images/01-d82506fa1159da1ed1d2e78f3e97b06c.jpg"},8878:(e,t,a)=>{a.d(t,{Z:()=>o});const o=a.p+"assets/images/02-444cf0b5c01b57bf21dba84e8f56bfed.jpg"},3882:(e,t,a)=>{a.d(t,{Z:()=>o});const o=a.p+"assets/images/03-d0863f8c1c1bd8da75e6b82fe67064b4.jpg"}}]);
